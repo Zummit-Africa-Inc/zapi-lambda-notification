ff54328df46009d58725d2d570630a5c460e8300
Chimexx <mezie.cjo@gmail.com>
2022-08-25 21:12:46 +0100

    Implemented Comms with core service via RabbitMq

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.setGlobalPrefix('zapi-notify')
+  app.setGlobalPrefix('zapi-notify');
 
-  if(process.env.NODE_ENV !== 'production'){
-     const options = new DocumentBuilder()
+  if (process.env.NODE_ENV !== 'production') {
+    const options = new DocumentBuilder()
       .setTitle('Zapi Lamda Notification Service')
       .setDescription('Zummit Africa API hub email & text notification')
       .setVersion('1.0')
       .addBearerAuth()
-      .build()
-    
-    const document = SwaggerModule.createDocument(app, options)
-    SwaggerModule.setup('zapi-notify', app, document)
+      .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api-doc', app, document);
   }
   await app.listen(Number(process.env.NODE_PORT) || 3002);
 }
