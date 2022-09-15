49be777f270052b34b9ed4d4d590a5a75d2b5460
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-16 00:47:16 +0100

    save notification dto

---
+++ src/notification/dto/save-notification.dto.ts
@@ -0,0 +1,22 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsString } from "class-validator";
+
+export class SaveNotificationDto {
+
+    @IsOptional()
+    @IsString()
+    @ApiProperty()
+    notification_title: string
+
+    @IsString()
+    @ApiProperty()
+    notification_detail: string
+
+    @IsString()
+    @ApiProperty()
+    developer_id: string
+
+    @IsString()
+    @ApiProperty()
+    event_id: string
+}
