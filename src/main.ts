29cca2a9c7c1254cd77d71d18e602b7c52e614c7
Chimexx <mezie.cjo@gmail.com>
2022-09-20 16:26:56 +0100

    Removed node_env check to run swagger ui

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,16 @@ async function bootstrap() {
   app.enableCors();
   app.setGlobalPrefix('zapi-notify');
 
-  if (process.env.NODE_ENV !== 'production') {
-    const options = new DocumentBuilder()
-      .setTitle('Zapi Lamda Notification Service')
-      .setDescription('Zummit Africa API hub email & text notification')
-      .setVersion('1.0')
-      .addBearerAuth()
-      .build();
+  const options = new DocumentBuilder()
+    .setTitle('Zapi Lamda Notification Service')
+    .setDescription('Zummit Africa API hub email & text notification')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
 
-    const document = SwaggerModule.createDocument(app, options);
-    SwaggerModule.setup('api-doc', app, document);
-  }
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(Number(process.env.NODE_PORT) || 3002);
 }
 bootstrap();
