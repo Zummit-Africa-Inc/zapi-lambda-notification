046a7994d3fb78f63970288fbace08eae4b4d43d
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-21 19:05:07 +0100

    added appropraite response

--- src/notification/notification.controller.ts
+++ src/notification/notification.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { ZuAppResponse } from 'src/common/helpers/response';
 import { SaveNotificationDto } from './dto/save-notification.dto';
 import { UpdateNotificationStatus } from './dto/update-notification-status.dto';
 import { NotificationService } from './notification.service';
@@ -14,7 +15,8 @@ export class NotificationController {
 
     @Post('saveNotificationToDb')
     async saveNotificationToDb(@Body() dto: SaveNotificationDto){
-        return await this.notificationService.saveNotificationToDb(dto)
+        const savedNotification = await this.notificationService.saveNotificationToDb(dto)
+        return ZuAppResponse.Ok(savedNotification, 'Notification saved', '201')
     }
 
     @Get('searchForUserNotifications:developerId')
@@ -36,6 +38,7 @@ export class NotificationController {
     async updateNotificationStatus(
         @Param('notificationId') notificationId: string,
         @Body() dto : UpdateNotificationStatus){
-        return await this.notificationService.updateNotificationStatus(notificationId, dto)
+        const updatedNotifications = await this.notificationService.updateNotificationStatus(notificationId, dto)
+        return ZuAppResponse.Ok(updatedNotifications, 'Notification status changed to read','200')
     }
 }
