18c1de60cc03c6d43f13e34ed5fc1add77a6845f
alahirajeffrey <alahirajeffrey@gmail.com>
2022-09-27 15:33:10 +0100

    setup typeorm and notification service

--- src/ws-notification/ws-notification.module.ts
+++ src/ws-notification/ws-notification.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Notification } from 'src/entities/notification.entity';
+import { NotificationService } from 'src/notification/notification.service';
 import { WsNotificationController } from './ws-notification.controller';
 import { NotificationsGateway } from './ws-notification.gateway';
 import { WsNotificationService } from './ws-notification.service';
 
 
 @Module({
-  providers: [NotificationsGateway, WsNotificationService],
+  imports:[TypeOrmModule.forFeature([Notification])],
+  providers: [NotificationsGateway, WsNotificationService, NotificationService],
   controllers: [WsNotificationController],
 })
 export class WsNotificationModule {}
